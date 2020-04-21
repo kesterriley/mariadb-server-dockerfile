34ab1dfa446faa34f0e077a77e2c415d82925366
Kester Riley <kesterriley@hotmail.com>
2020-04-21 17:02:32 +0100

    Modified logic for checking variable

--- start.sh
+++ start.sh
@@ -211,12 +211,11 @@ function standalone_install () {
        if [[ -n $slavegtidpos ]]; then
 
          if [[ -n $MASTERHOST ]]; then
+           echo "STOP SLAVE; SET GLOBAL gtid_slave_pos = $slavegtidpos;" > change_master_to.sql.in
+           echo "CHANGE MASTER TO master_use_gtid = slave_pos, MASTER_HOST='$MASTERHOST', MASTER_USER='mariadb', MASTER_PASSWORD='mariadb', MASTER_CONNECT_RETRY=10; START SLAVE;" >> change_master_to.sql.in
+         else
            echo "MASTERHOST is not set"
-           exit
-         fi
-
-         echo "STOP SLAVE; SET GLOBAL gtid_slave_pos = $slavegtidpos;" > change_master_to.sql.in
-         echo "CHANGE MASTER TO master_use_gtid = slave_pos, MASTER_HOST='$MASTERHOST', MASTER_USER='mariadb', MASTER_PASSWORD='mariadb', MASTER_CONNECT_RETRY=10; START SLAVE;" >> change_master_to.sql.in
+         fi 
        else
          echo "SLAVE POS IS EMPTY"
        fi
