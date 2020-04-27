0ca0d869d0f56a4be209f940c804cda6fefd32f1
Kester Riley <kesterriley@hotmail.com>
2020-04-27 12:04:23 +0100

    Updates to replication script

--- replication_configuration.sh
+++ replication_configuration.sh
@@ -5,17 +5,52 @@
 
 # Inputs: "remote master host", "remote master user","remote master password","local master host", "local master user","local master password"
 
-[[ ! -z "$1" ]] && lv_remote_master_host=$1 || echo "lv_remote_master_host is not set, check configuration."; exit
-[[ ! -z "$1" ]] && lv_remote_master_user=$1 || echo "lv_remote_master_user is not set, check configuration."; exit
-[[ ! -z "$1" ]] && lv_remote_master_password=$1 || echo "lv_remote_master_password is not set, check configuration."; exit
-[[ ! -z "$1" ]] && lv_local_master_host=$1 || echo "lv_local_master_host is not set, check configuration."; exit
-[[ ! -z "$1" ]] && lv_local_master_user=$1 || echo "lv_local_master_user is not set, check configuration."; exit
-[[ ! -z "$1" ]] && lv_local_master_password=$1 || echo "lv_local_master_password is not set, check configuration."; exit
 
-echo "SET GLOBAL gtid_slave_pos = $slavegtidpos;" > /var/lib/mysql/change_master_to_${lv_remote_master_host%%.*}.sql.in
-echo "CHANGE MASTER '${lv_remote_master_host%%.*}' TO master_use_gtid = slave_pos, MASTER_HOST='$lv_remote_master_host', MASTER_USER='$lv_remote_master_user', MASTER_PASSWORD='$lv_remote_master_password', MASTER_CONNECT_RETRY=10; START SLAVE '${lv_remote_master_host%%.*}';" >> /var/lib/mysql/change_master_to_${lv_remote_master_host%%.*}.sql.in
+if [[ ! -z "$1" ]]; then
+  lv_remote_master_host=$1
+else
+  echo "lv_remote_master_host is not set, check configuration."
+  exit
+fi
 
-if [[ -f /var/lib/mysql/change_master_to.sql.in ]]; then
+if [[ ! -z "$2" ]]; then
+  lv_remote_master_user=$2
+else
+  echo "lv_remote_master_user is not set, check configuration."
+  exit
+fi
+
+if [[ ! -z "$3" ]]; then
+  lv_remote_master_password=$3
+else
+  echo "lv_remote_master_host is not set, check configuration."
+  exit
+fi
+
+if [[ ! -z "$4" ]]; then
+  lv_local_master_host=$4
+else
+  echo "lv_local_master_host is not set, check configuration."
+  exit
+fi
+
+if [[ ! -z "$5" ]]; then
+  lv_local_master_user=$5
+else
+  echo "lv_local_master_user is not set, check configuration."
+  exit
+fi
+
+if [[ ! -z "$6" ]]; then
+  lv_local_master_password=$6
+else
+  echo "lv_local_master_password is not set, check configuration."
+  exit
+fi
+
+echo "CHANGE MASTER '${lv_remote_master_host%%.*}' TO master_use_gtid = slave_pos, MASTER_HOST='$lv_remote_master_host', MASTER_USER='$lv_remote_master_user', MASTER_PASSWORD='$lv_remote_master_password', MASTER_CONNECT_RETRY=10; START SLAVE '${lv_remote_master_host%%.*}';" > /var/lib/mysql/change_master_to_${lv_remote_master_host%%.*}.sql.in
+
+if [[ -f /var/lib/mysql/change_master_to_${lv_remote_master_host%%.*}.sql.in ]]; then
 
   echo "Initializing replication from clone position"
   mariadb -u$lv_local_master_user -p$lv_local_master_password -h 127.0.0.1 < /var/lib/mysql/change_master_to_${lv_remote_master_host%%.*}.sql.in || exit 1
